0eb0b2f7d677dd68b282140b6a16173af8abad3e
Sunghyun Ko <sunko@cx1r3s1.42seoul.kr>
2023-04-18 10:54:02 +0900

    23_04_18

---
+++ Lecture4/Dir/Dir.c
@@ -0,0 +1,23 @@
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define PRINT_ERR_EXIT(_msg) {perror(_msg); exit(1);}
+
+int main(void)
+{
+  if (mkdir("system", 0755) == -1)
+    PRINT_ERR_EXIT("system");
+
+  if(mkdir("programming", 0755) == -1)
+    PRINT_ERR_EXIT("programming");
+
+  if (rename("system", "sysyemProgramming") == -1)
+    PRINT_ERR_EXIT("sysyemProgramming");
+
+  if (rmdir("programming") == -1)
+    PRINT_ERR_EXIT("programming");
+
+  return (0);
+}
---
+++ Lecture4/DirOffset/DirOffset.c
@@ -0,0 +1,43 @@
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define PRINT_ERR_EXIT(_msg) \
+  {                          \
+      perror(_msg);          \
+      exit(1);               \
+  }                            
+
+int main(void)
+{
+  DIR *dp;
+  struct dirent *dent;
+
+  if ((dp = opendir("systemProgramming")) == NULL)
+      PRINT_ERR_EXIT("opendir");
+
+  printf("** Directory content **\n");
+  printf("Start Offset : %ld\n", telldir(dp));
+
+  while ((dent = readdir(dp)))
+  {
+    printf("Read : %s ", dent->d_name);
+    printf("Cur offset : %ld\n", telldir(dp));
+  }
+
+  printf("** Directory Pointer Rewind **\n");
+  rewinddir(dp);
+  printf("Cur offset : %ld\n", telldir(dp));
+
+  printf("** Move Directory Pointer **\n");
+  seekdir(dp, 1);
+  printf("Cur offset : %ld\n", telldir(dp));
+
+  dent = readdir(dp);
+  printf("Read %s ", dent->d_name);
+  printf("Next offset : %ld\n", telldir(dp));
+
+  closedir(dp);
+  return (0);
+}
